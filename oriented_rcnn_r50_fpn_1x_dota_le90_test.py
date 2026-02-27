a552b80fdee3735f63735477604b320a36ca9361
sugmudy <152680311+sugmudy@users.noreply.github.com>
2026-02-27 12:10:46 +0800

    Add files via upload

---
+++ oriented_rcnn_r50_fpn_1x_dota_le90_test.py
@@ -0,0 +1,209 @@
+_base_ = [
+    '../_base_/datasets/dotav1.py', '../_base_/schedules/schedule_1x.py',
+    '../_base_/default_runtime.py'
+]
+
+metainfo = dict(
+    classes=(
+        'plane', 'ship', 'storage-tank', 'baseball-diamond',
+        'tennis-court', 'basketball-court', 'ground-track-field',
+        'harbor', 'bridge', 'large-vehicle', 'small-vehicle',
+        'helicopter', 'roundabout', 'soccer-ball-field',
+        'swimming-pool', 'container-crane'
+    )
+)
+
+custom_imports = dict(
+    imports=['mmrotate.models.necks.FGAA_FPN'], 
+    allow_failed_imports=False
+)
+
+angle_version = 'le90'
+model = dict(
+    type='OrientedRCNN',
+    backbone=dict(
+        type='ResNet',
+        depth=50,
+        num_stages=4,
+        out_indices=(0, 1, 2, 3),
+        frozen_stages=1,
+        norm_cfg=dict(type='BN', requires_grad=True),
+        norm_eval=True,
+        style='pytorch',
+        init_cfg=dict(type='Pretrained', checkpoint='torchvision://resnet50')),
+    neck=dict(
+        type='ABMAFPN',
+        in_channels=[256, 512, 1024, 2048],
+        out_channels=256,
+        num_outs=5,
+        start_level=0,
+        end_level=-1,
+        add_extra_convs=False,        # 先不用额外 P6/P7 conv
+        relu_before_extra_convs=False,
+
+        # ===== BiFPN 结构 =====
+        num_bifpn_layers=0,           # 可以先设 2 或  3
+        use_separable_conv=False,
+
+        # ===== 前景分支 (弱监督) =====
+        fgam_enable=True,
+        # 建议在较高层做前景, 避免低层噪声太多
+        fgam_levels=[2,3,4],     # 对应 P4~P7 (因为 P3 是 level 0)
+        fgam_alpha=0.8,               # gating 强度, 0.5 ~ 1.0 之间可以试
+        fgam_fg_loss_weight=0.7,      # 前景弱监督 loss 权重
+
+        # ===== 多头注意力 =====
+        use_attn=False,
+        attn_levels=[ 2, 3, 4 ],        # 在 P3,P4,P5 做 attention, P6,P7 不做
+        attn_num_heads=4,
+        attn_embed_dim=256,           # 与 out_channels 一致
+        attn_dropout=0.1,
+        attn_beta=0.6,                # 前景 bias 强度
+        attn_orient_scale=0.7,        # 方向偏置强度
+    ),
+    rpn_head=dict(
+        type='OrientedRPNHead',
+         
+        in_channels=256,
+        feat_channels=256,
+        version=angle_version,
+        anchor_generator=dict(
+            type='AnchorGenerator',
+            scales=[8],
+            ratios=[0.5, 1.0, 2.0],
+            strides=[4, 8, 16, 32, 64]),
+        bbox_coder=dict(
+            type='MidpointOffsetCoder',
+            angle_range=angle_version,
+            target_means=[0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+            target_stds=[1.0, 1.0, 1.0, 1.0, 0.5, 0.5]),
+        loss_cls=dict(
+            type='CrossEntropyLoss', use_sigmoid=True, loss_weight=1.0),
+        loss_bbox=dict(
+            type='SmoothL1Loss', beta=0.1111111111111111, loss_weight=1.0)),
+    roi_head=dict(
+        type='OrientedStandardRoIHead',
+        bbox_roi_extractor=dict(
+            type='RotatedSingleRoIExtractor',
+            roi_layer=dict(
+                type='RoIAlignRotated',
+                out_size=7,
+                sample_num=2,
+                clockwise=True),
+            out_channels=256,
+            featmap_strides=[4, 8, 16, 32]),
+        bbox_head=dict(
+            type='RotatedShared2FCBBoxHead',
+            in_channels=256,
+            fc_out_channels=1024,
+            roi_feat_size=7,
+            num_classes=16,
+            bbox_coder=dict(
+                type='DeltaXYWHAOBBoxCoder',
+                angle_range=angle_version,
+                norm_factor=None,
+                edge_swap=True,
+                proj_xy=True,
+                target_means=(.0, .0, .0, .0, .0),
+                target_stds=(0.1, 0.1, 0.2, 0.2, 0.1)),
+            reg_class_agnostic=True,
+            loss_cls=dict(
+                type='CrossEntropyLoss', use_sigmoid=False, loss_weight=1.0),
+            loss_bbox=dict(type='SmoothL1Loss', beta=1.0, loss_weight=1.0))),
+    train_cfg=dict(
+        rpn=dict(
+            assigner=dict(
+                type='MaxIoUAssigner',
+                pos_iou_thr=0.7,
+                neg_iou_thr=0.3,
+                min_pos_iou=0.3,
+                match_low_quality=True,
+                ignore_iof_thr=-1),
+            sampler=dict(
+                type='RandomSampler',
+                num=256,
+                pos_fraction=0.5,
+                neg_pos_ub=-1,
+                add_gt_as_proposals=False),
+            allowed_border=0,
+            pos_weight=-1,
+            debug=False),
+        rpn_proposal=dict(
+            nms_pre=2000,
+            max_per_img=2000,
+            nms=dict(type='nms', iou_threshold=0.8),
+            min_bbox_size=0),
+        rcnn=dict(
+            assigner=dict(
+                type='MaxIoUAssigner',
+                pos_iou_thr=0.5,
+                neg_iou_thr=0.5,
+                min_pos_iou=0.5,
+                match_low_quality=False,
+                iou_calculator=dict(type='RBboxOverlaps2D'),
+                ignore_iof_thr=-1),
+            sampler=dict(
+                type='RRandomSampler',
+                num=512,
+                pos_fraction=0.25,
+                neg_pos_ub=-1,
+                add_gt_as_proposals=True),
+            pos_weight=-1,
+            debug=False)),
+    test_cfg=dict(
+        rpn=dict(
+            nms_pre=2000,
+            max_per_img=2000,
+            nms=dict(type='nms', iou_threshold=0.8),
+            min_bbox_size=0),
+        rcnn=dict(
+            nms_pre=2000,
+            min_bbox_size=0,
+            score_thr=0.05,
+            nms=dict(iou_thr=0.1),
+            max_per_img=2000)))
+
+img_norm_cfg = dict(
+    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
+train_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='LoadAnnotations', with_bbox=True),
+    dict(type='RResize', img_scale=(1024, 1024)),
+    dict(
+        type='RRandomFlip',
+        flip_ratio=[0.25, 0.25, 0.25],
+        direction=['horizontal', 'vertical', 'diagonal'],
+        version=angle_version),
+    dict(type='Normalize', **img_norm_cfg),
+    dict(type='Pad', size_divisor=32),
+    dict(type='DefaultFormatBundle'),
+    dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels'])
+]
+
+data_root = 'data/split_DOTA/'
+
+data = dict(
+    train=dict(
+        type='DOTADataset',
+        data_root=data_root,
+        ann_file='train/labelTxt/',
+        img_prefix='train/images/',
+        version='le90',
+        pipeline=train_pipeline),
+    val=dict(
+        type='DOTADataset',
+        data_root=data_root,
+        ann_file='val/labelTxt/',
+        img_prefix='val/images/',
+        version='le90'),
+    test=dict(
+        type='DOTADataset',
+        data_root=data_root,
+        ann_file='val/labelTxt/',
+        img_prefix='val/images/',
+        version='le90',
+        test_mode=True)
+)
+
+work_dir = './work_dirs6/oriented_rcnn_dota15_r50_fgamfpn_1x'
+optimizer = dict(lr=0.005)
